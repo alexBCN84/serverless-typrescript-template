d9dd22c8ebfd40b32caecedd70bf2f3bed9c70d3
Alejandro Gines <alejandro.gines.martinez@zalando.de>
2020-12-24 10:02:15 +0100

    add support for headers

--- src/handler.ts
+++ src/handler.ts
@@ -4,6 +4,10 @@ import 'source-map-support/register';
 
 export const hello: APIGatewayProxyHandler = async (event) => ({
   statusCode: 200,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': true,
+  },
   body: JSON.stringify({
     message: echo('Module aliasing is really the best'),
     input: event,
